d89a01c7e0326acfbec61f8b0e4479127cb9308f
Serhii Zhyhanov <zhiganov76@gmail.com>
2022-08-03 17:46:29 +0300

    виконав завдання 1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,38 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Change code below this line
+
+console.log(galleryItems);
+
+const refs = {
+  galleryListEl: document.querySelector('.gallery'),
+};
+
+const creatMarkupOfGalleryItems = galleryItems => {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+   <a class="gallery__item" href="${original}">
+     <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+`;
+    })
+    .join('');
+};
+
+refs.galleryListEl.innerHTML = creatMarkupOfGalleryItems(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionType: 'attr',
+  captionsPosition: 'bottom',
+  captionDelay: 250,
+  captionsData: 'alt',
+});
