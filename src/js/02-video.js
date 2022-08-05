b6c61d79c4eb25be13aae3e16480485d40965147
Serhii Zhyhanov <zhiganov76@gmail.com>
2022-08-05 13:21:27 +0300

    зробив завдання 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+const parsedCurrentTime = JSON.parse(currentTime);
+let parsedSeconds = Math.floor(parsedCurrentTime.seconds);
+console.log(parsedSeconds);
+player
+  .setCurrentTime(parsedSeconds)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
